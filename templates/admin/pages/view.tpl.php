01db2a54f2d1a511b8930fc2930d44ded17de5f2
franklin <waddinsohn@gmail.com>
2019-10-17 21:51:41 -0300

    Adicionando o editor de texto Trix editor

---
+++ templates/admin/pages/view.tpl.php
@@ -0,0 +1,30 @@
+<h3 class="mb-5">Administração de Páginas</h3>
+
+<div class="row">
+    <div class="col-3">
+        <div class="row">
+            <dt class="col-sm-4">Título</dt>
+            <dd class="col-sm-8">Página inicial</dd>
+            
+            <dt class="col-sm-4">URL</dt>
+            <dd class="col-sm-8">/ - <a href="/" target="blank">Abrir</a></dd>
+            
+            <dt class="col-sm-4">Criado em</dt>
+            <dd class="col-sm-8">2018-01-01 01:01</dd>
+            
+            <dt class="col-sm-4">Atualizado em</dt>
+            <dd class="col-sm-8">2018-01-01 01:01</dd>
+        </div>
+    </div>
+        
+    <div class="col bg-light">
+        <h3>Página inicial</h3>
+        <p>Esta é a página inicial do site</p>
+    </div>
+</div>
+
+<p>
+    <a href="" class=""></a>
+</p>
+
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
